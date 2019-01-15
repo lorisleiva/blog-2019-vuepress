40bf9510a41fc11e7ed7782cb48fd87320890280
Loris Leiva <loris.leiva@gmail.com>
2019-01-15 17:13:11 +0000

    :lipstick: Start from empty Layout

---
+++ .vuepress/config.js
@@ -0,0 +1,22 @@
+module.exports = {
+	title: 'Loris Leiva',
+	dest: './public',
+	themeConfig: {
+		domain: 'https://lorisleiva.com',
+		repo: 'https://github.com/lorisleiva/blog',
+		nav: [
+			{ text: 'Home', link: '/' },
+			{ text: 'CV', link: '/cv/' },
+			{ text: 'Persona', link: '/persona/' },
+		],
+		user: {
+            //
+		}
+	},
+	plugins: [
+        //
+	],
+	head: [
+        //
+	]
+}
