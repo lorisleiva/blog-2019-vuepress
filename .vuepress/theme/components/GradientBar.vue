1c791ac03f8dd37fd16e448036e5b98043730c81
Loris Leiva <loris.leiva@gmail.com>
2019-01-16 19:18:35 +0000

    :lipstick: Add gradient bar

--- .vuepress/theme/components/GradientBar.vue
+++ .vuepress/theme/components/GradientBar.vue
@@ -1,11 +1,31 @@
 <template>
-    <div>
-        //
-    </div>
+    <div class="h-3" :style="animatedGradient"></div>
 </template>
 
 <script>
 export default {
-    //
+    props: {
+        colors: { default: () => [ '#EE7752', '#E73C7E', '#23A6D5', '#23D5AB' ] },
+        time: { default: '15s' },
+        degree: { default: '120deg' },
+    },
+    computed: {
+        animatedGradient () {
+            const [first, second] = this.colors
+            let colors = [...this.colors, first, second]
+            return `
+                animation: movingGradient ${this.time} linear infinite;
+                background-size: ${colors.length * 100}% 100%;
+                background-image: linear-gradient(${this.degree}, ${colors.join(', ')});
+            `
+        }
+    },
 }
 </script>
+
+<style>
+@keyframes movingGradient { 
+    0% {background-position:0% 50%}
+    100% {background-position:100% 50%}
+}
+</style>
