d4434f75be893e8960bc243608702a41402a76fa
Loris Leiva <loris.leiva@gmail.com>
2019-01-21 08:33:39 +0000

    :lipstick: Reduce skill text for mobile

--- .vuepress/theme/global-components/Skills.vue
+++ .vuepress/theme/global-components/Skills.vue
@@ -1,8 +1,8 @@
 <template>
-    <div class="flex flex-wrap font-sans text-sm md:text-base">
+    <div class="flex flex-wrap font-sans text-xs sm:text-sm md:text-base">
         <div
             v-for="skill in skills" :key="skill.body"
-            class="flex items-center border rounded-full h-8 px-3 mr-3 mb-3"
+            class="flex items-center border rounded-full h-6 sm:h-8 px-3 mr-3 mb-3"
             :class="classesFor(skill.theme)"
         >
             <Icon class="w-3 md:w-4 mr-2 mt-1" v-if="skill.icon" :icon="skill.icon"></Icon>
