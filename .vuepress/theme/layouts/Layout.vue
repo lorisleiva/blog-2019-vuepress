85b1880020aa399b352aebc12a79aed25dd0ee95
Loris Leiva <loris.leiva@gmail.com>
2019-01-27 16:07:58 +0000

    :pencil: Add (but don't use) sub layout distribution

--- .vuepress/theme/layouts/Layout.vue
+++ .vuepress/theme/layouts/Layout.vue
@@ -10,6 +10,14 @@ import Header from '@theme/components/Header'
 
 export default {
     components: { Header },
+    computed: {
+        layout () {
+            if (this.$page.regularPath.startsWith('/articles')) {
+                return 'ArticleLayout'
+            }
+            return this.$page.frontmatter.use || 'PageLayout'
+        }
+    }
 }
 </script>
 
