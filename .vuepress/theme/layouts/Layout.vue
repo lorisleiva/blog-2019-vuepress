509cb57403d5bb3448ccdca98a3cf2229fb6bbac
Loris Leiva <loris.leiva@gmail.com>
2019-02-05 17:14:38 +0000

    :lipstick: Add footer

--- .vuepress/theme/layouts/Layout.vue
+++ .vuepress/theme/layouts/Layout.vue
@@ -2,18 +2,22 @@
     <div class="theme-container">
         <Header />
         <component :is="layout" />
+        <Footer />
     </div>
 </template>
 
 <script>
 import { isArticle } from '@theme/utils'
 import Header from '@theme/components/Header'
+import Footer from '@theme/components/Footer'
+
+// Available layouts.
 import HomeLayout from '@theme/layouts/HomeLayout'
 import ArticleLayout from '@theme/layouts/ArticleLayout'
 import BasicLayout from '@theme/layouts/BasicLayout'
 
 export default {
-    components: { Header, HomeLayout, ArticleLayout, BasicLayout },
+    components: { Header, Footer, HomeLayout, ArticleLayout, BasicLayout },
     computed: {
         layout () {
             if (this.$page.path === '/') return 'HomeLayout'
