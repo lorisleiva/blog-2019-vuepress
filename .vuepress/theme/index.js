b14354ba0b6f0c996809418ab76f404b033521b4
Loris Leiva <loris.leiva@gmail.com>
2019-02-06 17:46:53 +0000

    :arrow_up: Add Google Analytics

--- .vuepress/theme/index.js
+++ .vuepress/theme/index.js
@@ -1,10 +1,11 @@
 const path = require('path')
 
 module.exports = {
-	plugins: [
-        'seo',
-        'disqus',
-    ],
+	plugins: {
+        '@vuepress/google-analytics': { ga: 'UA-100767601-5' },
+        'seo': true,
+        'disqus': true,
+    },
     enhanceAppFiles: [
         path.resolve(__dirname, 'articles.js'),
         path.resolve(__dirname, 'search.js'),
