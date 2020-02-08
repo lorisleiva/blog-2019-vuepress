0fd7023566592231b181f984199cdb53e860e0b7
Loris Leiva <loris.leiva@gmail.com>
2020-02-08 22:20:44 +0000

    wip

--- .vuepress/theme/components/Navigator.vue
+++ .vuepress/theme/components/Navigator.vue
@@ -1,8 +1,7 @@
 <template>
-    <div class="fixed inset-0 z-40 flex items-start h-full" v-if="openned">
-        <div class="absolute inset-0 bg-white opacity-90" @click="close"></div>
-        <div class="relative mx-auto mt-32 w-full max-w-xl">
-
+    <div class="fixed inset-0 z-40 px-4 overflow-auto" v-if="openned">
+        <div class="fixed inset-0 bg-white opacity-90" @click="close"></div>
+        <div class="relative mx-auto mt-32 mb-16 w-full max-w-xl">
             <div class="border border-gray-300 shadow-xl rounded-lg">
                 <input
                     ref="input"
@@ -15,30 +14,27 @@
                     @keydown.up.prevent="move(-1)"
                     @keydown.down.prevent="move(1)"
                 >
-
-                <div class="bg-gray-100 border-t border-gray-300 rounded-b-lg p-4" @mouseleave="unfocus">
+                <div class="bg-white border-t border-gray-300 rounded-b-lg p-4" @mouseleave="unfocus">
                     <a 
                         v-for="(page, index) in suggestions"
                         :key="page.key + (page.header ? `_${page.header.slug}` : '')"
-                        class="flex px-4 py-2 text-lg font-semibold text-gray-600 hover:text-gray-600 rounded cursor-pointer border-0"
-                        :class="index === focused ? 'bg-topaz' : ''"
+                        class="flex items-center px-4 py-2 text-lg font-semibold text-gray-600 hover:text-gray-600 rounded cursor-pointer border-0"
+                        :class="index === focused ? 'bg-gray-200' : ''"
                         :href="page.path"
                         @click="go($event, index)"
                         @mouseenter="focus(index)"
                     >
                         <Icon
-                            class="w-5 h-5 mr-3 mt-2px"
+                            class="w-5 h-5 mr-3 mb-px"
                             :icon="iconForPage(page)" 
-                            :primary="index === focused ? 'text-white' : 'text-gray-600'"
-                            :secondary="index === focused ? 'text-gray-400' : 'text-gray-800'"
+                            :primary="index === focused ? 'text-gray-700' : 'text-gray-600'"
+                            :secondary="index === focused ? 'text-black' : 'text-gray-800'"
                         ></Icon>
-                        <div :class="index === focused ? 'text-white' : ''">
+                        <div :class="index === focused ? 'text-black' : ''">
                             <div v-text="page.title"></div>
                             <span v-if="page.header" class="text-sm">&rightarrow;&nbsp;{{ page.header.title }}</span>
                         </div>
                     </a>
-
-                    <!-- No results -->
                     <div 
                         v-if="query && suggestions.length === 0"
                         class="p-6 text-center text-gray-600"
@@ -56,7 +52,7 @@ import { fetchPagesInArray, isArticle } from '@theme/utils'
 export default {
     data () {
         return {
-            openned: true,
+            openned: false,
             focused: 0,
             query: '',
         }
@@ -73,12 +69,14 @@ export default {
     methods: {
         open () {
             this.openned = true
+            document.querySelector('body').classList.add('overflow-hidden')
             this.$nextTick(() => {
                 this.$refs.input.focus()
             })
         },
         close () {
             this.openned = false
+            document.querySelector('body').classList.remove('overflow-hidden')
             this.reset()
         },
         go (event, index) {
@@ -90,7 +88,7 @@ export default {
             index = typeof index === 'undefined' ? this.focused : index
             if (typeof this.suggestions[index] === 'undefined') return
             this.$router.push(this.suggestions[index].path)
-            this.reset()
+            this.close()
         },
         reset () {
             this.focused = 0
