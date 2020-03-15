00ea8939a8ea8508edd46729efaea816298c0a0f
Loris Leiva <loris.leiva@gmail.com>
2020-03-15 16:08:18 +0000

    Increase gradient timer

--- .vuepress/theme/components/AnimatedGradient.vue
+++ .vuepress/theme/components/AnimatedGradient.vue
@@ -6,7 +6,7 @@
 export default {
     props: {
         colors: { default: () => [ '#EE7752', '#E73C7E', '#23A6D5', '#23D5AB' ] },
-        time: { default: '15s' },
+        time: { default: '25s' },
         degree: { default: '120deg' },
         progress: { default: 100 },
     },
